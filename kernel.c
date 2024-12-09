32ba5a9c3553dd9b94421e7a6b95b053e2cb2e30
Braden Helmer <bradenhelmeraus@gmail.com>
2024-12-08 19:06:20 -0500

    Initial Commit

---
+++ kernel.c
@@ -0,0 +1,18 @@
+#include "kernel.h"
+
+volatile u8 *uart = (u8 *)0x09000000;
+
+void putchar(char c) {
+  *uart = c;
+}
+
+void print(const char *s) {
+  while (*s != '\0') {
+	putchar(*s);
+	s++;
+  }
+}
+
+void kmain(void) {
+  print("BHARMOS Kernel\n");
+}
---
+++ kernel.h
@@ -0,0 +1,19 @@
+// kernel.h
+// ~~~~~~~~
+// Common kernel definitions.
+
+// clang-format off
+// Integer Typedefs
+typedef unsigned char  u8;
+typedef unsigned short u16;
+typedef unsigned int   u32;
+typedef unsigned long  u64;
+typedef signed   char  s8;
+typedef signed   short s16;
+typedef signed   int   s32;
+typedef signed   long  s64;
+
+// Floating Point Typedefs
+typedef float       f32;
+typedef double      f64;
+typedef long double f128;
